d6301edde72e734ad79aeeb96e5246c879a8e79d
Higor Bezerra Tomaz <higordesign@hotmail.com>
2024-10-14 16:14:54 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.desafio.dominio.Bootcamp;
 import br.com.desafio.dominio.Curso;
+import br.com.desafio.dominio.Dev;
 import br.com.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -24,8 +26,41 @@ public class  Main {
         mentoria.setDescricao("descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devHigor = new Dev();
+        devHigor.setNome("Higor");
+        devHigor.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Higor: " + devHigor.getConteudosInscritos());
+        devHigor.progredir();
+        devHigor.progredir();
+        System.out.println("_");
+        System.out.println("Conteúdos Inscritos Higor: " + devHigor.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos Higor: " + devHigor.getConteudosConcluidos());
+        System.out.println("XP: " + devHigor.calcularTotalXp());
+
+        System.out.println("__________________________");
+
+        Dev devBenjamim = new Dev();
+        devBenjamim.setNome("Benjamim");
+        devBenjamim.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscrits Benjamim: " + devBenjamim.getConteudosInscritos());
+        devBenjamim.progredir();
+        devBenjamim.progredir();
+        devBenjamim.progredir();
+        System.out.println("_");
+        System.out.println("Conteúdos Inscritos Benjamim: " + devHigor.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos Benjamim: " + devHigor.getConteudosConcluidos());
+        System.out.println("XP: " + devBenjamim.calcularTotalXp());
     }
 }
